2bdc5c0a90dde64e011c9d76c0657fb3180eba85
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-02 15:36:55 -0500

    Added product images

--- src/data/products.js
+++ src/data/products.js
@@ -1,56 +1,54 @@
 // Import product images (Assuming you have images named Product1.jpg to Product25.jpg in your assets folder)
-import Product1 from '../assets/black-calacatta-polished.jpg';
-import Product2 from '../assets/black-calacatta-polished.jpg';
-import Product3 from '../assets/black-calacatta-polished.jpg';
-import Product4 from '../assets/black-calacatta-polished.jpg';
-import Product5 from '../assets/black-calacatta-polished.jpg';
-import Product6 from '../assets/black-calacatta-polished.jpg';
-import Product7 from '../assets/black-calacatta-polished.jpg';
-import Product8 from '../assets/black-calacatta-polished.jpg';
-import Product9 from '../assets/black-calacatta-polished.jpg';
-import Product10 from '../assets/black-calacatta-polished.jpg';
-import Product11 from '../assets/black-calacatta-polished.jpg';
-import Product12 from '../assets/black-calacatta-polished.jpg';
-import Product13 from '../assets/black-calacatta-polished.jpg';
-import Product14 from '../assets/black-calacatta-polished.jpg';
-import Product15 from '../assets/black-calacatta-polished.jpg';
-import Product16 from '../assets/black-calacatta-polished.jpg';
-import Product17 from '../assets/black-calacatta-polished.jpg';
-import Product18 from '../assets/black-calacatta-polished.jpg';
-import Product19 from '../assets/black-calacatta-polished.jpg';
-import Product20 from '../assets/black-calacatta-polished.jpg';
-import Product21 from '../assets/black-calacatta-polished.jpg';
-import Product22 from '../assets/black-calacatta-polished.jpg';
-import Product23 from '../assets/black-calacatta-polished.jpg';
-import Product24 from '../assets/black-calacatta-polished.jpg';
-import Product25 from '../assets/black-calacatta-polished.jpg';
+import Product1 from '../assets/products/nickelblue-fq-0001.jpg';
+import Product2 from '../assets/products/capri-ivory-matte-fq-0002.jpg';
+import Product3 from '../assets/products/nano-ultra-white-fq-0003.jpg';
+import Product4 from '../assets/products/onyx-cielo-fq-0004.jpg';
+import Product5 from '../assets/products/argon-white-polished-fq-0005.jpg';
+import Product6 from '../assets/products/julian-jade-polished-fq-0006.jpg';
+import Product7 from '../assets/products/pietresossa-marble-semi-fq-0007.jpg';
+import Product8 from '../assets/products/aurora-light-matte-fq-0008.jpg';
+import Product9 from '../assets/products/aurora-medium-matte-fq-0009.jpg';
+import Product10 from '../assets/products/aurora-dark-matte-fq-0010.jpg';
+import Product11 from '../assets/products/timeless-light-matte-fq-0011.jpg';
+import Product12 from '../assets/products/timeless-medium-fq-0012.jpg';
+import Product13 from '../assets/products/timeless-dark-matte-fq-0013.jpg';
+import Product14 from '../assets/products/bloom-light-matte-fq-0015.jpg';
+import Product15 from '../assets/products/poyraz-blanco-matte-fq-0016.jpg';
+import Product16 from '../assets/products/portland-matte-fq-0018.jpg';
+import Product17 from '../assets/products/nightlight-matte-fq-0019.jpg';
+import Product18 from '../assets/products/hardvein-matte-fq-0020.jpg';
+import Product19 from '../assets/products/cement-grigio-matte-fq-0021.jpg';
+import Product20 from '../assets/products/icebreak-matte-fq-0022.jpg';
+import Product21 from '../assets/products/black-calacatta-polished-fq-0023.jpg';
+import Product22 from '../assets/products/siearra-vizon-fq-0024.jpg';
+import Product23 from '../assets/products/infinity-matte-fq-0101.jpg';
+import Product24 from '../assets/products/octavia-matte-fq-0102.jpg';
 
 const products = [
-  { image: Product1, name: 'Tile Name 1', code: 'FQ-0001', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product2, name: 'Tile Name 2', code: 'FQ-0002', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product3, name: 'Tile Name 3', code: 'FQ-0003', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product4, name: 'Tile Name 4', code: 'FQ-0004', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product5, name: 'Tile Name 5', code: 'FQ-0005', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product1, name: 'Tile Name 1', code: 'FQ-0001', type: 'Semi', details: 'Details about the FQ Product.' },
+  { image: Product2, name: 'Tile Name 2', code: 'FQ-0002', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product3, name: 'Tile Name 3', code: 'FQ-0003', type: 'Polished', details: 'Details about the FQ Product.' },
+  { image: Product4, name: 'Tile Name 4', code: 'FQ-0004', type: 'Semi', details: 'Details about the FQ Product.' },
+  { image: Product5, name: 'Tile Name 5', code: 'FQ-0005', type: 'Polished', details: 'Details about the FQ Product.' },
   { image: Product6, name: 'Tile Name 6', code: 'FQ-0006', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product7, name: 'Tile Name 7', code: 'FQ-0007', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product8, name: 'Tile Name 8', code: 'FQ-0008', type: 'Polished', details: 'Details about the FQ Product.' },
+  { image: Product7, name: 'Tile Name 7', code: 'FQ-0007', type: 'Semi', details: 'Details about the FQ Product.' },
+  { image: Product8, name: 'Tile Name 8', code: 'FQ-0008', type: 'Matte', details: 'Details about the FQ Product.' },
   { image: Product9, name: 'Tile Name 9', code: 'FQ-0009', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product10, name: 'Tile Name 10', code: 'FQ-0010', type: 'Polished', details: 'Details about the FQ Product.' },
+  { image: Product10, name: 'Tile Name 10', code: 'FQ-0010', type: 'Matte', details: 'Details about the FQ Product.' },
   { image: Product11, name: 'Tile Name 11', code: 'FQ-0011', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product12, name: 'Tile Name 12', code: 'FQ-0012', type: 'Polished', details: 'Details about the FQ Product.' },
+  { image: Product12, name: 'Tile Name 12', code: 'FQ-0012', type: 'Matte', details: 'Details about the FQ Product.' },
   { image: Product13, name: 'Tile Name 13', code: 'FQ-0013', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product14, name: 'Tile Name 14', code: 'FQ-0014', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product15, name: 'Tile Name 15', code: 'FQ-0015', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product16, name: 'Tile Name 16', code: 'FQ-0016', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product17, name: 'Tile Name 17', code: 'FQ-0017', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product18, name: 'Tile Name 18', code: 'FQ-0018', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product19, name: 'Tile Name 19', code: 'FQ-0019', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product20, name: 'Tile Name 20', code: 'FQ-0020', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product21, name: 'Tile Name 21', code: 'FQ-0021', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product22, name: 'Tile Name 22', code: 'FQ-0022', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product23, name: 'Tile Name 23', code: 'FQ-0023', type: 'Matte', details: 'Details about the FQ Product.' },
-  { image: Product24, name: 'Tile Name 24', code: 'FQ-0024', type: 'Polished', details: 'Details about the FQ Product.' },
-  { image: Product25, name: 'Tile Name 25', code: 'FQ-0025', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product14, name: 'Tile Name 14', code: 'FQ-0015', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product15, name: 'Tile Name 15', code: 'FQ-0016', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product16, name: 'Tile Name 16', code: 'FQ-0018', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product17, name: 'Tile Name 17', code: 'FQ-0019', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product18, name: 'Tile Name 18', code: 'FQ-0020', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product19, name: 'Tile Name 19', code: 'FQ-0021', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product20, name: 'Tile Name 20', code: 'FQ-0022', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product21, name: 'Tile Name 21', code: 'FQ-0023', type: 'Polished', details: 'Details about the FQ Product.' },
+  { image: Product22, name: 'Tile Name 22', code: 'FQ-0024', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product23, name: 'Tile Name 23', code: 'FQ-0101', type: 'Matte', details: 'Details about the FQ Product.' },
+  { image: Product24, name: 'Tile Name 24', code: 'FQ-0102', type: 'Matte', details: 'Details about the FQ Product.' },
 ];
 
 export default products;
